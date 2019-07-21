8a0e1477f1a878fd81caad85ada82d757143b18f
Ramesh77 <49685362+laliya7@users.noreply.github.com>
2019-07-21 21:46:36 +0545

    addition feature

--- main.java
+++ main.java
@@ -3,5 +3,10 @@ public class helloWorld {
   public static void main(String[] args) {
         
         System.out.println("Hello, world!");
+    int a,b;
+    a=8;
+    b=9;
+    int sum=a+b;
+    System.out.print("Total: "+sum);
   }
 }
